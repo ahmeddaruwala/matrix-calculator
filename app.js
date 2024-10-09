992fa53b648a4f0d9fd5425b1fa5f846bbd3ecbc
ahmeddaruwala <ahmeddaruwala360@gmail.com>
2024-10-09 20:31:05 +0500

    matrix calculator using js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const form = document.getElementById("matrix-form");
-const boxes1 = document.getElementById("inp-boxes1");
-const boxes2 = document.getElementById("inp-boxes2");
-const addBtn = document.getElementById("add");
-const result = document.getElementById("result-box");
+const form = document.querySelector("#matrix-form");
+const boxes1 = document.querySelector("#inp-boxes1");
+const boxes2 = document.querySelector("#inp-boxes2");
+const addBtn = document.querySelector("#add");
+const subBtn = document.querySelector("#sub");
+const resultDiv = document.querySelector("#result-box");
 
 let matrixLength;
 
@@ -13,12 +14,11 @@ form.addEventListener("submit", (event) => {
 
   boxes1.innerHTML = "";
   boxes2.innerHTML = "";
-  result.innerHTML = "";
 
   for (let i = 0; i < matrixLength; i++) {
     for (let j = 0; j < matrixLength; j++) {
-      boxes1.innerHTML += `<input type="number" id="mat-box1${i}${j}" style="width: 30px;" value="0">`;
-      boxes2.innerHTML += `<input type="number" id="mat-box2${i}${j}" style="width: 30px;" value="0">`;
+      boxes1.innerHTML += `<input type="number" id="mat-box1${i}${j}" style="width: 30px; margin: 3px;" value="0">`;
+      boxes2.innerHTML += `<input type="number" id="mat-box2${i}${j}" style="width: 30px; margin: 3px;" value="0">`;
     }
     boxes1.innerHTML += `<br>`;
     boxes2.innerHTML += `<br>`;
@@ -26,22 +26,40 @@ form.addEventListener("submit", (event) => {
 });
 
 addBtn.addEventListener("click", () => {
-  const sum_of_Array = [];
-
-  for (let i = 0; i < matrixLength; i++) {
-    let addedValue = [];
-    for (let j = 0; j < matrixLength; j++) {
-      const value1 = Number(document.getElementById(`mat-box1${i}${j}`).value);
-
-      const value2 = Number(document.getElementById(`mat-box2${i}${j}`).value);
-
-      addedValue.push(value1 + value2);
-    }
-    sum_of_Array.push(addedValue);
-  }
-  result.innerHTML = "<h5>Sum of both Matrices: <h5>";
-
-  for (let i = 0; i < sum_of_Array.length; i++) {
-    result.innerHTML += sum_of_Array[i].join(" ") + `<br>`;
-  }
+  addMatrix()
 });
+  const addMatrix = () => {
+    resultDiv.innerHTML = "";
+
+    for (let i = 0; i < matrixLength; i++) {
+      for (let j = 0; j < matrixLength; j++) {
+        const value1 = Number (document.querySelector(`#mat-box1${i}${j}`).value);
+        const value2 = Number (document.querySelector(`#mat-box2${i}${j}`).value);
+
+        const result = Number (value1) + Number (value2);
+
+        resultDiv.innerHTML += `<input type="number" style="width: 30px; margin: 3px;" value="${result}">`;
+      }
+      resultDiv.innerHTML += "<br/>";
+    }
+  };
+
+subBtn.addEventListener("click", () => {
+  subMatrix()
+});
+
+  const subMatrix = () => {
+    resultDiv.innerHTML = "";
+
+    for (let i = 0; i < matrixLength; i++) {
+      for (let j = 0; j < matrixLength; j++) {
+        const value1 = Number (document.getElementById(`mat-box1${i}${j}`).value);
+        const value2 = Number (document.getElementById(`mat-box2${i}${j}`).value);                
+
+        const result = Number (value1) - Number (value2);
+
+        resultDiv.innerHTML += `<input type="number" style="width: 30px; margin: 3px;" value="${result}">`;
+      }
+      resultDiv.innerHTML += "<br/>";
+    }
+  };
